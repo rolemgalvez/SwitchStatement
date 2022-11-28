82f6ab5c7d3decf5b29dd1b5934db7fce32bb4b6
Rolem Galvez <rolemgalvez@gmail.com>
2022-11-29 07:55:00 +0800

    feature: add PhilHealthContribution project

---
+++ PhilHealthContribution/Program.cs
@@ -0,0 +1,37 @@
+﻿//Welcome message
+Console.WriteLine("PhilHealth Contribution 2022");
+Console.WriteLine("-----------------------");
+Console.WriteLine();
+
+//Get user input
+Console.Write("Enter your Monthly Basic Salary: ");
+double.TryParse(Console.ReadLine(), out double monthlyBasicSalary);
+double philHealthContribution = double.NaN;
+
+//Show result
+Console.WriteLine();
+switch (monthlyBasicSalary)
+{
+	case 10000:
+		philHealthContribution = 400;
+        break;
+	case >= 10000.01 and <= 79999.99:
+        philHealthContribution = monthlyBasicSalary * 0.04;
+		break;
+	case >= 80000:
+		philHealthContribution = 3200;
+        break;
+	default:
+		Console.WriteLine("You are not qualified to have PhilHealth Contribution");
+		break;
+}
+
+if (double.IsNaN(philHealthContribution) == false)
+{
+	Console.WriteLine("Your Monthly PhilHealth Contribution is as follows:");
+	Console.WriteLine($"Employer Share     : {philHealthContribution / 2}");
+	Console.WriteLine($"Employee Share     : {philHealthContribution / 2}");
+	Console.WriteLine($"Total Contribution : {philHealthContribution}");
+}
+
+Console.ReadLine();
